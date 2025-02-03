edd83e328ea9bb6c0c69fd0305cd8484c4e370be
Camelia Ciuca <camelia1414@gmail.com>
2025-02-03 16:03:19 +0100

    tests

--- src/example.py
+++ src/example.py
@@ -1,3 +1,17 @@
 
 def add(x,y):
     return x + y
+
+def compare_names(input, name):
+    if not isinstance(input, str):
+        return False
+    if not isinstance(name, str):
+        return False
+    name_small = name.casefold()
+    input_small = input.casefold()
+    position = name_small.casefold().find(input_small)
+    if position == -1:
+        return False
+    else:
+        return True
+
--- tests/test_example.py
+++ tests/test_example.py
@@ -1,4 +1,4 @@
-from src.example import add
+from src.example import add, compare_names
 
 
 def test_add_positive_numbers():
@@ -8,3 +8,21 @@ def test_add_positive_numbers():
 
 def test_fail():
     assert True
+
+def test_compareNames__input_is_not_a_string():
+    expect = False
+    actual = compare_names(123,"Name")
+    assert expect == actual
+
+
+def test_compareNames__input_is_not_a_string():
+    expect = False
+    actual = compare_names("kal",123)
+    assert expect == actual
+
+def test_compareNames__input_is_in_name():
+    expect = True
+    actual = compare_names("kal","Kalle")
+    assert expect == actual
+
+#TODO expected false om namnen inte matchar
