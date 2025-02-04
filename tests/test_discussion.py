2ba7e5f88a93bc7ff1a534df2d682b14e79e7193
Camelia Ciuca <camelia1414@gmail.com>
2025-02-04 12:38:24 +0100

    test_number

--- tests/test_discussion.py
+++ tests/test_discussion.py
@@ -1,7 +1,22 @@
-from src.discussion import bigger_than_100
+from src.discussion import bigger_than_100, number_equal_with_42
 
 
 def test_bigger_than_100():
     expected = False
     actual = bigger_than_100(1)
     assert expected == actual
+
+def test_number_equal_with_42():
+    expected = "Number 42 is equal with '42'"
+    actual = number_equal_with_42(42)  # Call the function!
+    assert expected == actual
+
+    expected_less = "Number 41 is less than '42'"
+    actual_less = number_equal_with_42(41)
+    assert expected_less == actual_less
+
+    expected_more = "Number 43 is more than '42'"
+    actual_more = number_equal_with_42(43)
+    assert expected_more == actual_more
+
+
